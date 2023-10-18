71a9473bbefe1e2dadcff3fac03344ba29b916ca
KUMAREPKM <kumar.epkm@gmail.com>
2023-10-18 20:02:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using DesignPattern;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        // Get the singleton instance of SingletonClass
+        SingletonClass singletonClass = SingletonClass.getInstance();
+
+        try
+        {
+            while (true)
+            {
+                Console.Write("Enter a message (or type 'exit' to quit): ");
+                string message = Console.ReadLine();
+
+                // Exit loop if user types 'exit' or provides an empty message
+                if (string.IsNullOrEmpty(message) || message.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                // Log the entered message
+                singletonClass.LogEntryMessage(message);
+            }
+
+            Console.WriteLine("Logging complete. Press any key to exit.");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
---
+++ SingletonClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DesignPattern
+{
+    /// <summary>
+    /// Represents a singleton class for logging messages to a file.
+    /// </summary>
+    public class SingletonClass
+    {
+        string logFilePath = "bhlog.txt"; // Path to the log file.
+
+        StreamWriter writer; // Object for writing to the file.
+
+        // Singleton instance created when class is loaded.
+        private static SingletonClass singleTonObj = new SingletonClass();
+
+        // Private constructor ensures only one instance can be created.
+        private SingletonClass()
+        {
+            // Create a file stream and a writer to append to the log file.
+            FileStream fs = new FileStream(logFilePath, FileMode.Append, FileAccess.Write);
+            writer = new StreamWriter(fs);
+        }
+
+        /// <summary>
+        /// Gets the singleton instance of SingletonClass.
+        /// </summary>
+        /// <returns>The singleton instance.</returns>
+        public static SingletonClass getInstance()
+        {
+            return singleTonObj;
+        }
+
+        /// <summary>
+        /// Logs an entry message with a timestamp to the log file.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
+        public void LogEntryMessage(string message)
+        {
+            writer.WriteLine(DateTime.Now + ": " + message); // Write message along with timestamp.
+            writer.Flush(); // Ensure the message is written immediately.
+        }
+
+        /// <summary>
+        /// Destructor that ensures the writer is closed when the object is garbage collected.
+        /// </summary>
+        ~SingletonClass()
+        {
+            writer.Close();
+        }
+    }
+}
